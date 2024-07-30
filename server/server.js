691021ae6e242e5a93e2885323efe2118b95b0be
AM-ML <aliredamoumneh@gmail.com>
2024-07-30 03:01:48 +0300

    Set up: /signin, /signup routes

--- server/server.js
+++ server/server.js
@@ -1,9 +1,126 @@
 import express from "express";
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { nanoid } from "nanoid";
+import "dotenv/config";
+
+// schema
+import User from "./Schema/User.js";
 
 const server = express();
+let port = 3000;
 
-const port = 3000;
+let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
+let passwordRegex = /^(?=.*\d)(?=.*[a-z]).{6,20}$/; // regex for password
+
+// use middleware to enable json data sharing between request and response
+server.use(express.json());
+
+// connected to mongoDB project database 
+mongoose.connect(process.env.DB_LOCATION, {
+  autoIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 50000 // increase timeout to 50 seconds
+});
+
+const generateUsername = async (email) => {
+  let username = email.split('@')[0];
+
+  let usernameNotUnique = await User.exists({ "personal_info.username": username }).then((result) => result);
+
+  // nanoid(): random unique id / number
+  return usernameNotUnique ? username += nanoid().substring(0, 5) : username;
+}
+
+const formatDataToSend = (user) => {
+  const access_token = jwt.sign({ id: user._id }, process.env.SECRET_ACCESS_KEY);
+  
+  return {
+    access_token,
+    profile_img: user.personal_info.profile_img,
+    username: user.personal_info.username,
+    name: user.personal_info.name
+  }
+}
+
+// if post request to /signup
+server.post("/signup", (req, res) => {
+  let { name, email, password } = req.body;
+
+  /* data validation process */
+
+  // full name
+  if (name.length < 5) {
+    // 403: invalidation code
+    return res.status(403).json({ "error": "Full name must be atleast 5 letters long." });
+  }
+
+  //email
+  if (!email.length) {
+    return res.status(403).json({ "error": "Enter Email" });
+  }
+
+  if (!emailRegex.test(email)) {
+    return res.status(403).json({ "error": "Invalid Email" });
+  }
+  if (!passwordRegex.test(password)) {
+    return res.status(403).json({ "error": "Password must be 6 to 20 characters with a numeric and 1 lowercase letter " })
+  }
+
+  /* storing process */
+  //.hash(pwd, iterations, callback function)
+  bcrypt.hash(password, 10, async (err, hashed_pwd) => { // encrypting pwd
+    let username = await generateUsername(email); // creating username
+
+    let user = new User({ // creating user object from mongoose userSchema imported from User.js
+      personal_info: {
+        name,
+        email,
+        password: hashed_pwd,
+        username
+      }
+    });
+
+    user.save().then((u) => { // storing user object and returning the mongoDB user object
+      return res.status(200).json(formatDataToSend(u));
+    })
+      .catch(err => {
+        if (err.code == 11000) { // 11000: unique object duplicated error
+          return res.status(500).json({ "error": "Email already exists" });
+        }
+
+        return res.status(500).json({ "error": err.message });
+      })
+  })
+})
+
+server.post("/signin", (req, res) => {
+  const {email, password} = req.body;
+
+  User.findOne({ "personal_info.email": email })
+  .then((user) => {
+    if(!user) {
+      return res.status(403).json({"error": "Email not found"});
+    }
+
+    bcrypt.compare(password, user.personal_info.password, (err, result) => {
+      if (err) {
+        return res.status(403).json({"error": "error occured while login, please try again"})
+      } if (!result) {
+        return res.status(403).json({"error": "password is incorrect"});
+      }
+
+      return res.status(200).json(formatDataToSend(user));
+    });
+  })
+  .catch(err => {
+    console.log(err);
+    return res.status(500).json({"error": err.message});
+  })
+})
 
 server.listen(port, () => {
-  console.log("listening on port "+port)
+  console.log("listening on port " + port);
 })    
