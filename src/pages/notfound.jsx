bebc3c0125ae63cef4d2b8e1a11708c8aa86d75c
AM-ML <aliredamoumneh@gmail.com>
2024-08-07 17:29:44 +0300

    + 404 not found page

---
+++ src/pages/notfound.jsx
@@ -0,0 +1,10 @@
+import AnimationWrapper from "../common/page-animation";
+import NotFoundLayout from "../components/notfoundlayout";
+
+const NotFound = () => {
+  return <AnimationWrapper>
+    <NotFoundLayout />
+  </AnimationWrapper>
+}
+
+export default NotFound;
