ab84538a3f3f059195cedb4c8d55dae95a90cad0
AM-ML <aliredamoumneh@gmail.com>
2024-08-08 15:39:30 +0300

    + blog publishing base files: to publish the blog you wrote...

---
+++ src/components/editorpublish.jsx
@@ -0,0 +1,15 @@
+import "../css/components/editorpublish.css";
+
+const EditorPublishForm = () => {
+  return (
+    <div className="epf-container">
+      <div className="epf-gi">
+        <h1 className="epf-title">
+          BLOG PUBLISHING FORM
+        </h1>
+      </div>
+    </div>
+  )
+}
+
+export default EditorPublishForm;
