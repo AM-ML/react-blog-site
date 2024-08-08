a74bd5f022ff7a6e668287eab50900297a38a574
AM-ML <aliredamoumneh@gmail.com>
2024-08-08 15:40:02 +0300

    mod editor.jsx: switch smoothly between editing and publishing a blog

--- src/pages/editor.jsx
+++ src/pages/editor.jsx
@@ -1,10 +1,17 @@
+import { useState } from "react"
 import AnimationWrapper from "../common/page-animation"
 import EditorPanel from "../components/editor-panel"
+import EditorPublishForm from "../components/editorpublish";
 
 const Editor = () => {
+  const [editorState, setEditorState] = useState("editor");
   return (
     <AnimationWrapper>
-      <EditorPanel />
+      { editorState == "editor" ?
+          <EditorPanel />
+            :
+          <EditorPublishForm />
+      }
     </AnimationWrapper>
   )
 }
