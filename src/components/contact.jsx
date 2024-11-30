e6c801dcc6a9421ef30b67bf5d22d9168db4184b
am-ml <aliredamoumneh@gmail.com>
2024-11-30 18:20:20 +0200

    contact-us page layout bug fix

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -8,7 +8,8 @@ const Contact = ({}) => {
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3311.6157525276535!2d35.48783130560243!3d33.89954861044447!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151f1747e8de30e5%3A0xaa466541c35880ec!2sBOFFO%20Consulting%20Group!5e0!3m2!1sen!2slb!4v1732979594167!5m2!1sen!2slb"
           width="100%"
-          height="450"
+          height="calc(100vh - 100px)"
+          min-height="450"
           allowfullscreen=""
           loading="lazy"
           referrerpolicy="no-referrer-when-downgrade"
